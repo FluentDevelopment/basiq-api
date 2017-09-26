1442fc9a24c4698de4489daaf18b838a37ae6c1b
Bradly Sharpe <bradly@fluentdevelopment.com.au>
2017-09-26 16:16:27 +1000

    Rewrite core and use resources

---
+++ lib/client.ts
@@ -0,0 +1,159 @@
+import * as axios from 'axios';
+import * as debug from 'debug';
+import { decode } from 'jws';
+import * as querystring from 'qs';
+
+import { BasiqAPIOptions, BasiqResponse, JwtToken } from './interfaces';
+
+const { name, version, homepage } = require('./../package.json');
+
+const log = debug('basiq-api:client');
+export class Client {
+  private userAgent = `${name.split('/').pop()}/${version} node/${process.version} (${homepage})`;
+  private options: BasiqAPIOptions;
+  private token: JwtToken;
+  private request: axios.AxiosInstance;
+  private DEFAULTS: BasiqAPIOptions = {
+    baseUrl: 'https://au-api.basiq.io',
+    auth: {
+      apiKey: '',
+    },
+  };
+  constructor(options: BasiqAPIOptions) {
+    this.options = Object.assign({}, this.DEFAULTS, options);
+
+    if (!this.options.auth.apiKey.length) {
+      log('API Key not passed to constructor');
+      throw new Error('API Key must be passed');
+    }
+
+    this.request = axios.default.create({
+      baseURL: this.options.baseUrl,
+      headers: {
+        'User-Agent': this.userAgent,
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+    });
+
+    this.setBaseUrl(this.options.baseUrl);
+  }
+
+  setBaseUrl(baseUrl: string = ''): Client {
+    if (baseUrl.length && baseUrl.slice(-1) === '/') {
+      baseUrl = baseUrl.replace(/\/$/, '');
+    }
+
+    this.options.baseUrl = baseUrl;
+    this.request.defaults.baseURL = this.options.baseUrl;
+
+    return this;
+  }
+
+  formatResponse(res: axios.AxiosResponse): BasiqResponse {
+    return {
+      status: res.status,
+      statusText: res.statusText,
+      headers: res.headers,
+      body: res.data,
+    };
+  }
+
+  async get(url: string, config?: axios.AxiosRequestConfig): Promise<axios.AxiosResponse> {
+    config = await this.prepareConfig(config);
+    log('Prepared Config', config);
+    return await this.request
+      .get(this.prependSlash(url), config)
+      ;
+  }
+
+  async post(url: string, data?: any, config?: axios.AxiosRequestConfig): Promise<axios.AxiosResponse> {
+    config = await this.prepareConfig(config);
+    return await this.request
+      .post(this.prependSlash(url), data, config)
+      ;
+  }
+
+  async put(url: string, data?: any, config?: axios.AxiosRequestConfig): Promise<axios.AxiosResponse> {
+    config = await this.prepareConfig(config);
+    return await this.request
+      .put(this.prependSlash(url), data, config)
+      ;
+  }
+
+  async delete(url: string, config?: axios.AxiosRequestConfig): Promise<axios.AxiosResponse> {
+    config = await this.prepareConfig(config);
+    return await this.request
+      .delete(this.prependSlash(url), config)
+      ;
+  }
+
+  private prependSlash(url: string = ''): string {
+    if (url.length && url[0] !== '/') {
+      url = `/${url}`;
+    }
+    return url;
+  }
+
+  private async prepareConfig(config?: axios.AxiosRequestConfig): Promise<axios.AxiosRequestConfig> {
+    await this.checkToken();
+    return this.setCommonHeaders(config);
+  }
+
+  private async checkToken(): Promise<JwtToken> {
+    let haveValidToken = false;
+
+    if (this.token) {
+      const decodedAccessToken: any = decode(this.token.access_token);
+      const tokenExpiration = new Date(decodedAccessToken.exp * 1000);
+      haveValidToken = (new Date()).getTime() <= tokenExpiration.getTime();
+
+      log('Token valid?', haveValidToken ? 'Yes' : 'No');
+    } else {
+      log('No token');
+    }
+
+
+    if (!haveValidToken) {
+      log('Refreshing token');
+      await this.authenticate()
+        .then(res => this.token);
+    } else {
+      return this.token;
+    }
+  }
+
+  private async authenticate(): Promise<axios.AxiosResponse> {
+    log('Authenticating');
+    const data = {
+      'grant_type': 'client_credentials',
+    };
+
+    const headers = {
+      'Content-Type': 'application/x-www-form-urlencoded',
+      'Accept': 'application/json',
+      'Authorization': `Basic ${this.options.auth.apiKey}`,
+    };
+
+    return await this.request
+      .post(`/oauth2/token`, querystring.stringify(data), { headers })
+      .then(res => {
+        // Save Access Token
+        this.token = res.data;
+        log('Set token', this.token);
+        return res;
+      })
+      ;
+  }
+
+  private async setCommonHeaders(config?: axios.AxiosRequestConfig): Promise<axios.AxiosRequestConfig> {
+    const token = await this.checkToken();
+    log('Setting Headers');
+    const headers = {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json',
+      'Authorization': (this.token ? `${this.token.token_type} ${this.token.access_token}` : ''),
+    };
+    return Object.assign({}, config, { headers });
+  }
+}
--- lib/index.ts
+++ lib/index.ts
@@ -1,92 +1,57 @@
+import * as auto from 'autocreate';
 import * as debug from 'debug';
-import * as jwtdecode from 'jwt-decode';
-import * as request from 'superagent';
-import * as superagentPrefix from 'superagent-prefix';
 
-import { BasiqApiError } from './error';
-import { AuthenticationOptions, BasiqAPIOptions, JwtToken } from './interfaces';
+import { Client } from './client';
+import { BasiqAPIOptions } from './interfaces';
+import { Account } from './resources/accounts';
+import { Connection } from './resources/connections';
+import { Institution } from './resources/institutions';
+import { Transaction } from './resources/transactions';
 
 const log = debug('basiq-api');
 
-export class BasiqAPI {
-  private token: JwtToken;
-  private request: request.SuperAgentStatic;
-  private prefix: request.Plugin;
+const resources = {
+  connections: Connection,
+  accounts: Account,
+  transactions: Transaction,
+  institutions: Institution,
+};
 
-  options: BasiqAPIOptions;
+const Basiq = auto(class BasiqAPI {
+  protected _client: Client;
 
-  constructor(opts: BasiqAPIOptions) {
-    this.request = request;
+  constructor(options: BasiqAPIOptions) {
+    const client = new Client(options);
 
-    this.options = opts;
+    // log('Adding client', client);
+    Object.defineProperty(this, '_client', {
+      value: client,
+      writable: false,
+      enumerable: false,
+      configurable: false,
+    });
 
-    // Remove trailing slash from Base URL
-    this.options.baseURL = this.options.baseURL.replace(/\/$/, '');
-    this.prefix = superagentPrefix(this.options.baseURL);
+    Object.keys(resources)
+      .forEach((resource: string) => {
+        // log('Adding property', resource, this);
+        Object.defineProperty(this, resource, {
+          value: new resources[resource](this._client),
+          writable: true,
+          enumerable: false,
+          configurable: true,
+        });
+      });
   }
 
-  private async getNewToken(): Promise<JwtToken> {
-    return await request
-      .post(`/oauth2/token`)
-      .use(this.prefix)
-      .send({
-        'grant_type': 'client_credentials'
-      })
-      .set({
-        'Content-Type': 'application/x-www-form-urlencoded',
-        'Accept': 'application/json',
-        'Authorization': `Basic ${this.options.auth.apiKey}`
-      })
-      .then(res => res.body as JwtToken)
-      .catch((error) => {
-        if (error && error.response) {
-          log('Basiq API Error', {
-            status: error.status,
-            message: error.message,
-            body: error.response.body
-          });
+});
 
-          throw new BasiqApiError(error.status, error.message, error.response);
-        } else throw error;
-      })
-      ;
-  }
+export {
+  BasiqAPIOptions,
+  AuthenticationOptions,
+  ConnectionCreateOptions,
+  ConnectionUpdateOptions,
+  BasiqResponse,
+  BasiqPromise,
+} from './interfaces';
 
-  private async refreshTokenIfExpired() {
-
-    const updateToken = async () => {
-      await this.getNewToken()
-        .then((token: JwtToken) => this.token = token)
-        .catch(error => { throw error })
-        ;
-    }
-
-    if (this.token) {
-      const decodedAccessToken: JwtToken = jwtdecode(this.token.access_token) as JwtToken;
-      const tokenExpiration = new Date(decodedAccessToken.expires_in * 1000);
-      if ((new Date()).getTime() > tokenExpiration.getTime()) {
-        log(`Token has expired; Fetch new`);
-        await updateToken()
-          .catch(error => { throw error })
-          ;
-      }
-    } else {
-      log(`No token; Fetch new`);
-      await updateToken()
-        .catch(error => { throw error })
-        ;
-    }
-  }
-
-  async authenticate(auth?: AuthenticationOptions): Promise<JwtToken> {
-    this.options.auth = auth || this.options.auth;
-    return await this
-      .refreshTokenIfExpired()
-      .then(() => log('Got auth token', this.token))
-      .then(() => this.token)
-      .catch(error => { throw error })
-      ;
-  }
-
-
-}
+export { Basiq };
