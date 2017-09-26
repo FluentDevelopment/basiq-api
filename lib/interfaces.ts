a5204469db0e9f4e746b7d9f547e5e36cc1c5630
Bradly Sharpe <bradly@fluentdevelopment.com.au>
2017-09-26 16:15:24 +1000

    Create common interfaces and types

--- lib/interfaces.ts
+++ lib/interfaces.ts
@@ -3,7 +3,7 @@ export interface AuthenticationOptions {
 }
 
 export interface BasiqAPIOptions {
-  baseURL: string;
+  baseUrl?: string;
   auth: AuthenticationOptions;
 }
 
@@ -12,3 +12,37 @@ export interface JwtToken {
   token_type: string;
   expires_in: number;
 }
+
+export interface JwtTokenDecoded {
+  partnerid: string;
+  applicationid: string;
+  exp: number;
+  iat: number;
+  version: string;
+}
+
+export interface ConnectionCreateOptions {
+  loginId: string;
+  password: string;
+  securityCode?: string;
+  externalUserId: string;
+  institution: {
+    id: string;
+  };
+}
+
+export interface ConnectionUpdateOptions {
+  loginId?: string;
+  password?: string;
+  securityCode?: string;
+  externalUserId?: string;
+}
+
+export interface BasiqResponse {
+  status: number;
+  statusText: string;
+  body: any;
+  headers?: any;
+}
+
+export type BasiqPromise = Promise<BasiqResponse>;
