0c5272ac6ad4f64f85ec49a331fd9c5d9a64ff18
Bradly Sharpe <bradly@fluentdevelopment.com.au>
2017-09-26 19:12:21 +1000

    Remove autocreate dependency and output resource types

--- lib/index.ts
+++ lib/index.ts
@@ -1,4 +1,3 @@
-import * as auto from 'autocreate';
 import * as debug from 'debug';
 
 import { Client } from './client';
@@ -17,33 +16,46 @@ const resources = {
   institutions: Institution,
 };
 
-const Basiq = auto(class BasiqAPI {
+class Basiq {
+
+  public accounts: Account;
+
+  public connections: Connection;
+
+  public transactions: Transaction;
+
+  public institutions: Institution;
+
   protected _client: Client;
 
   constructor(options: BasiqAPIOptions) {
-    const client = new Client(options);
+    if (this instanceof Basiq) {
+      const client = new Client(options);
 
-    // log('Adding client', client);
-    Object.defineProperty(this, '_client', {
-      value: client,
-      writable: false,
-      enumerable: false,
-      configurable: false,
-    });
-
-    Object.keys(resources)
-      .forEach((resource: string) => {
-        // log('Adding property', resource, this);
-        Object.defineProperty(this, resource, {
-          value: new resources[resource](this._client),
-          writable: true,
-          enumerable: false,
-          configurable: true,
-        });
+      // log('Adding client', client);
+      Object.defineProperty(this, '_client', {
+        value: client,
+        writable: false,
+        enumerable: false,
+        configurable: false,
       });
+
+      Object.keys(resources)
+        .forEach((resource: string) => {
+          // log('Adding property', resource, this);
+          Object.defineProperty(this, resource, {
+            value: new resources[resource](this._client),
+            writable: true,
+            enumerable: false,
+            configurable: true,
+          });
+        });
+    } else {
+      return new Basiq(options);
+    }
   }
 
-});
+}
 
 export {
   BasiqAPIOptions,
